22a6011641a6b0c4bc5f2beed41c702b8431bc90
Hassen-Ahmed <hassenbest23@gmail.com>
2023-07-31 15:22:33 +0100

    create global form style

--- src/styles/Styling.tsx
+++ src/styles/Styling.tsx
@@ -7,6 +7,17 @@ export const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "white",
   },
+  form: {
+    borderWidth: 0.2,
+    gap: 30,
+    padding: 50,
+    paddingVertical: 70,
+    borderColor: "black",
+    borderRadius: 2,
+    marginBottom: 50,
+
+    backgroundColor: "white",
+  },
   text_input_label: {
     fontSize: 20,
     marginBottom: 7,
