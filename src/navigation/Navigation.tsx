f2c35789fe5f6a501ad516a5a7e5471216899849
JacvaSipt <steamcow12@hotmail.com>
2023-07-31 14:05:57 +0100

    create route for Sign in screen

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -2,21 +2,23 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import Welcome from "../screens/welcome/Welcome";
+import SignIn from "../screens/welcome/SignIn";
 
 const Stack = createNativeStackNavigator();
 
 export default function Navigation() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen
-                    name="Welcome"
-                    component={Welcome}
-                    options={{
-                        headerShown: false,
-                    }}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen name="Signin" component={SignIn} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
