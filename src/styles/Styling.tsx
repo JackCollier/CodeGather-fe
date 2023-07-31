01f71ded69ae5b4665e5aeed273c696c595428c5
JacvaSipt <steamcow12@hotmail.com>
2023-07-31 14:23:24 +0100

    create global text input and label styling

--- src/styles/Styling.tsx
+++ src/styles/Styling.tsx
@@ -6,4 +6,17 @@ export const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  text_input_label: {
+    fontSize: 20,
+    marginBottom: 7,
+  },
+  text_input: {
+    borderWidth: 1,
+    padding: 3,
+    borderRadius: 0,
+    minWidth: "75%",
+    fontSize: 20,
+    height: 40,
+    borderColor: "#8cb3d9",
+  },
 });
