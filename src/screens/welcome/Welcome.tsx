d61086a50dd71c3c93c020e92af2ec0803be00d6
Hassen-Ahmed <hassenbest23@gmail.com>
2023-07-31 12:27:31 +0100

    create basic welcome page styling

--- src/screens/welcome/Welcome.tsx
+++ src/screens/welcome/Welcome.tsx
@@ -1,14 +1,20 @@
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import { View, Text, SafeAreaView, StyleSheet, Pressable } from "react-native";
 import { styles } from "../../styles/Styling";
 
 const Welcome = () => {
     return (
         <SafeAreaView style={styles.outerContainer}>
             <View>
-                <Text>CodeGather</Text>
+                <Text style={WelcomeStyles.logo}>{"<CodeGather/>"}</Text>
             </View>
-            <View>
-                
+            <View style={WelcomeStyles.welcome_btn_container}>
+                <Pressable style={WelcomeStyles.welcome_btn}>
+                    <Text style={WelcomeStyles.welcome_btn_text}>Login</Text>
+                </Pressable>
+
+                <Pressable style={WelcomeStyles.welcome_btn}>
+                    <Text style={WelcomeStyles.welcome_btn_text}>Signup</Text>
+                </Pressable>
             </View>
         </SafeAreaView>
     );
@@ -16,6 +22,25 @@ const Welcome = () => {
 
 const WelcomeStyles = StyleSheet.create({
     container: styles.outerContainer,
+    logo: {
+        fontSize: 40,
+        fontWeight: "bold",
+        letterSpacing: 2,
+    },
+    welcome_logo_container: {},
+    welcome_btn_container: {
+        backgroundColor: "red",
+    },
+    welcome_btn: {
+        paddingHorizontal: 8,
+        paddingVertical: 4,
+        backgroundColor: "#8cb3d9",
+        borderRadius: 4,
+    },
+    welcome_btn_text: {
+        fontSize: 20,
+        textAlign: "center",
+    },
 });
 
 export default Welcome;
