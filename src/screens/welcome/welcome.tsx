c129b87f1e4357d17aaf6af21c156d43a61e705c
JacvaSipt <steamcow12@hotmail.com>
2023-07-31 10:43:49 +0100

    create Welcome component structure

--- src/screens/welcome/welcome.tsx
+++ src/screens/welcome/welcome.tsx
@@ -1,5 +1,10 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import { styles } from "../../styles/styling";
 
-const Welcome = () => {};
+const Welcome = () => {
+  <SafeAreaView style={styles.outerContainer}></SafeAreaView>;
+};
+
+const WelcomeStyles = StyleSheet.create({});
 
 export default Welcome;
