812e000c0d3f80e2e8efcde44d6d5fae5153206c
JacvaSipt <steamcow12@hotmail.com>
2023-07-31 15:00:58 +0100

    add further styling

--- src/screens/welcome/SignIn.tsx
+++ src/screens/welcome/SignIn.tsx
@@ -12,21 +12,45 @@ import { styles } from "../../styles/Styling";
 
 export default function SignIn() {
   return (
-    <SafeAreaView style={styles.outerContainer}>
+    <SafeAreaView
+      style={{
+        ...styles.outerContainer,
+        backgroundColor: "#8cb3d9",
+      }}
+    >
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
-        keyboardVerticalOffset={Platform.OS === "ios" ? 120 : 0}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 80 : 0}
       >
         <View style={SignInStyles.signin_form}>
           <View>
-            <Text style={styles.text_input_label}>Email</Text>
-            <TextInput style={styles.text_input} placeholder="Email..." />
+            <Text style={{ ...styles.text_input_label }}>Email</Text>
+            <TextInput
+              style={{ ...styles.text_input, backgroundColor: "white" }}
+              placeholder="Email..."
+            />
           </View>
           <View>
-            <Text style={styles.text_input_label}>Password</Text>
-            <TextInput style={styles.text_input} placeholder="Password..." />
+            <Text style={{ ...styles.text_input_label }}>Password</Text>
+            <TextInput
+              style={{
+                ...styles.text_input,
+                borderWidth: 0.5,
+                borderRadius: 1,
+              }}
+              placeholder="Password..."
+            />
           </View>
-          <Pressable style={styles.btn}>
+          <Pressable
+            style={{
+              ...styles.btn,
+              backgroundColor: "#8cb3d9",
+              borderWidth: 0.5,
+              width: "75%",
+              alignSelf: "center",
+              borderColor: "#8cb3d9",
+            }}
+          >
             <Text style={styles.btn_text}>Sign in</Text>
           </Pressable>
         </View>
@@ -37,11 +61,14 @@ export default function SignIn() {
 
 const SignInStyles = StyleSheet.create({
   signin_form: {
-    borderWidth: 1,
+    borderWidth: 0.2,
     gap: 30,
-    padding: 30,
-    borderColor: "#8cb3d9",
+    padding: 50,
+    paddingVertical: 70,
+    borderColor: "black",
     borderRadius: 2,
     marginBottom: 50,
+
+    backgroundColor: "white",
   },
 });
