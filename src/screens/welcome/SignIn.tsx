318f5b43eb6848999e569b6dbf90d6afd326c6e8
JacvaSipt <steamcow12@hotmail.com>
2023-07-31 14:33:30 +0100

    add keyboard avoid view tag

--- src/screens/welcome/SignIn.tsx
+++ src/screens/welcome/SignIn.tsx
@@ -5,28 +5,32 @@ import {
   StyleSheet,
   Pressable,
   TextInput,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import { styles } from "../../styles/Styling";
 
 export default function SignIn() {
   return (
     <SafeAreaView style={styles.outerContainer}>
-      <View style={SignInStyles.signin_form}>
-        <View>
-          <Text style={styles.text_input_label}>Email</Text>
-          <TextInput
-            style={styles.text_input}
-            placeholder="FakeEmail@Fmail.com"
-          />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 120 : 0}
+      >
+        <View style={SignInStyles.signin_form}>
+          <View>
+            <Text style={styles.text_input_label}>Email</Text>
+            <TextInput style={styles.text_input} placeholder="Email..." />
+          </View>
+          <View>
+            <Text style={styles.text_input_label}>Password</Text>
+            <TextInput style={styles.text_input} placeholder="Password..." />
+          </View>
+          <Pressable style={styles.btn}>
+            <Text style={styles.btn_text}>Sign in</Text>
+          </Pressable>
         </View>
-        <View>
-          <Text style={styles.text_input_label}>Password</Text>
-          <TextInput
-            style={styles.text_input}
-            placeholder="NotARealPassword..."
-          />
-        </View>
-      </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
@@ -35,8 +39,9 @@ const SignInStyles = StyleSheet.create({
   signin_form: {
     borderWidth: 1,
     gap: 30,
-    padding: 20,
+    padding: 30,
     borderColor: "#8cb3d9",
     borderRadius: 2,
+    marginBottom: 50,
   },
 });
