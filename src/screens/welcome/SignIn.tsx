fbb45a4749ca743c3e0fe77f404ffe41362cd4ab
JacvaSipt <steamcow12@hotmail.com>
2023-07-31 14:23:05 +0100

    create sign in form and basic styling

--- src/screens/welcome/SignIn.tsx
+++ src/screens/welcome/SignIn.tsx
@@ -1,5 +1,42 @@
-import { View, Text, SafeAreaView, StyleSheet, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  Pressable,
+  TextInput,
+} from "react-native";
+import { styles } from "../../styles/Styling";
 
 export default function SignIn() {
-  return <SafeAreaView></SafeAreaView>;
+  return (
+    <SafeAreaView style={styles.outerContainer}>
+      <View style={SignInStyles.signin_form}>
+        <View>
+          <Text style={styles.text_input_label}>Email</Text>
+          <TextInput
+            style={styles.text_input}
+            placeholder="FakeEmail@Fmail.com"
+          />
+        </View>
+        <View>
+          <Text style={styles.text_input_label}>Password</Text>
+          <TextInput
+            style={styles.text_input}
+            placeholder="NotARealPassword..."
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const SignInStyles = StyleSheet.create({
+  signin_form: {
+    borderWidth: 1,
+    gap: 30,
+    padding: 20,
+    borderColor: "#8cb3d9",
+    borderRadius: 2,
+  },
+});
