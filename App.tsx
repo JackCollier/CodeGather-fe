b0ae5069232f389aa1500a19f1cce7f06bd228f8
JacvaSipt <steamcow12@hotmail.com>
2023-08-01 11:12:13 +0100

    format with prettier

--- App.tsx
+++ App.tsx
@@ -3,14 +3,14 @@ import { StyleSheet, Text, View } from "react-native";
 import Navigation from "./src/navigation/Navigation";
 
 export default function App() {
-    return <Navigation />;
+  return <Navigation />;
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
