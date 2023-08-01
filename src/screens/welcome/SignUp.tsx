99a2e5210c50a2877f4bb3c12cab0e1fe0fc3c2a
JacvaSipt <steamcow12@hotmail.com>
2023-08-01 10:06:39 +0100

    replace Picker component with Select List and implement with mock data

--- src/screens/welcome/SignUp.tsx
+++ src/screens/welcome/SignUp.tsx
@@ -9,10 +9,26 @@ import {
   Platform,
   ScrollView,
 } from "react-native";
-import { Picker } from "@react-native-picker/picker";
+import { SelectList } from "react-native-dropdown-select-list";
 import { styles } from "../../styles/Styling";
+import { useState } from "react";
 
 export default function SignUp() {
+  const [selected, setSelected] = useState("");
+
+  const mockLocationData = [
+    { key: 1, value: "London" },
+    { key: 2, value: "Liverpool" },
+    { key: 3, value: "Manchester" },
+    { key: 4, value: "Manchester" },
+    { key: 5, value: "Manchester" },
+    { key: 6, value: "Manchester" },
+    { key: 7, value: "Manchester" },
+    { key: 8, value: "Manchester" },
+    { key: 9, value: "Manchester" },
+    { key: 10, value: "Mewport" },
+  ];
+
   return (
     <SafeAreaView
       style={{
@@ -55,12 +71,14 @@ export default function SignUp() {
             </View>
             <View>
               <Text style={{ ...styles.text_input_label }}>Your Location</Text>
-              <Picker>
-                <Picker.Item label="London" value="London" />
-                <Picker.Item label="Mancheser" value="Manchester" />
-                <Picker.Item label="Liverpool" value="Liverpool" />
-                <Picker.Item label="Sheffield" value="Sheffield" />
-              </Picker>
+              <SelectList
+                setSelected={(val: string) => setSelected(val)}
+                data={mockLocationData}
+                save="value"
+                boxStyles={{ borderColor: "#8cb3d9" }}
+                dropdownStyles={{ borderColor: "#8cb3d9" }}
+                maxHeight={110}
+              />
             </View>
             <Pressable style={styles.btn}>
               <Text style={styles.btn_text}>Sign up</Text>
