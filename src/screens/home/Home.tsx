ef87abdff66daa31a48c4c61cf1a75d8e5e34150
Hassen-Ahmed <hassenbest23@gmail.com>
2023-08-01 13:35:23 +0100

    add basic layouts for home and styling

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,12 +1,40 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+// import {SafeAreaView} from 'react-native-safe-area-context'
 import React from "react";
+import { styles } from "../../styles/Styling";
 
 export default function Home() {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Home</Text>
+      <View style={styles.outerContainer}>
+        <View style={homeStyles.nav_container}>
+          <Text>Nav</Text>
+        </View>
+        <View style={homeStyles.horizontal_list_container}>
+          <Text>Horizonatal FlatList</Text>
+        </View>
+        <View style={homeStyles.vertical_list_container}>
+          <Text>Vertical FlatList</Text>
+        </View>
       </View>
     </SafeAreaView>
   );
 }
+
+const homeStyles = StyleSheet.create({
+  nav_container: {
+    flex: 0.1,
+    borderColor: "orange",
+    borderWidth: 2,
+  },
+  horizontal_list_container: {
+    flex: 0.5,
+    borderColor: "orange",
+    borderWidth: 2,
+  },
+  vertical_list_container: {
+    flex: 0.5,
+    borderColor: "orange",
+    borderWidth: 2,
+  },
+});
