ae78ef276ee97eb7045a7cb03542b76c244bef4d
Hassen-Ahmed <hassenbest23@gmail.com>
2023-08-02 10:45:54 +0100

    create basic react native structure

--- src/screens/home/SingleEventPage.tsx
+++ src/screens/home/SingleEventPage.tsx
@@ -1,3 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
 export default function SingleEventPage() {
-  return <div>SingleEventPage</div>;
+  return (
+    <View>
+      <Text>SingleEventPage</Text>
+    </View>
+  );
 }
