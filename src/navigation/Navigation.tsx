b6b3ac1667770367a04b2c03194e125c24547b2e
Hassen-Ahmed <hassenbest23@gmail.com>
2023-08-03 16:18:59 +0100

    create HostEvent page screen and add logo for tabs

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,6 +1,9 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import { FontAwesome } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { StyleSheet } from "react-native";
 
 import Welcome from "../screens/welcome/Welcome";
 import SignIn from "../screens/welcome/SignIn";
@@ -9,13 +12,35 @@ import Home from "../screens/home/Home";
 import SingleEventPage from "../screens/home/SingleEventPage";
 import Profile from "../screens/profile/Profile";
 import Location from "../screens/location/Location";
+import HostEvents from "../screens/host_events/HostEvents";
 
 const Stack = createNativeStackNavigator();
 const BottomTab = createBottomTabNavigator();
 
 function Tab() {
   return (
-    <BottomTab.Navigator>
+    <BottomTab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: () => {
+          if (route.name === "HostEvents") {
+            return (
+              <MaterialIcons name="event-available" size={32} color="#3c8eba" />
+            );
+          } else if (route.name === "Home") {
+            return <FontAwesome name="home" size={32} color="#3c8eba" />;
+          } else if (route.name === "Host Event") {
+            return <MaterialIcons name="event" size={32} color="#3c8eba" />;
+          } else if (route.name === "Profile") {
+            return <FontAwesome name="user-circle" size={32} color="#3c8eba" />;
+          }
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+        tabBarStyle: {
+          ...stackScreenStyles.tab_style,
+        },
+      })}
+    >
       <BottomTab.Screen
         name="Home"
         component={Home}
@@ -24,6 +49,13 @@ function Tab() {
         }}
       />
 
+      <BottomTab.Screen
+        name="HostEvents"
+        component={HostEvents}
+        options={{
+          headerShown: false,
+        }}
+      />
       <BottomTab.Screen
         name="Profile"
         component={Profile}
@@ -81,3 +113,16 @@ export default function Navigation() {
     </NavigationContainer>
   );
 }
+
+const stackScreenStyles = StyleSheet.create({
+  tab_style: {
+    position: "absolute",
+    bottom: -1,
+    left: 0,
+    right: 0,
+    height: 60,
+    paddingHorizontal: 10,
+    paddingBottom: 5,
+    paddingTop: 5,
+  },
+});
