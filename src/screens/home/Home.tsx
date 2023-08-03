d158c188224d739300b26c1dc77acb5feb1b3aac
JacvaSipt <steamcow12@hotmail.com>
2023-08-03 15:46:54 +0100

    replace mock data with api call to back end data

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -17,104 +17,15 @@ import {
   Article,
   Profile,
 } from "../../utils/RenderFunctions";
+import { getEventData } from "../../utils/CodeGatherApi";
 
 export default function Home({ navigation }: any) {
   const [articles, setArticles] = useState<Article[]>([]);
 
   useEffect(() => {
-    setArticles([
-      {
-        event_id: 1,
-        title: "React Study Group",
-        username: "jeffTheOnlyCodeGod",
-        event_img_url:
-          "https://solguruz.com/wp-content/uploads/2022/09/ReactJS-Framework-Benefits.png",
-        location: "Manchester",
-        date: "26/09/2023",
-        description:
-          "Join our React Study Group to learn and discuss React concepts and best practices.",
-        topics: ["React", "Frontend", "Web Development"],
-        attending: [{ userName: "ben" }],
-        size_limit: 6,
-      },
-      {
-        event_id: 2,
-        title: "Python Coders Conference",
-        username: "python_master",
-        event_img_url:
-          "https://i.natgeofe.com/n/7fef9761-077c-45d0-9cca-78a984b9d614/burmese-python_thumb_4x3.jpg",
-        location: "San Francisco",
-        date: "15/10/2023",
-        description:
-          "The Python Coders Conference is a gathering of Python enthusiasts to share knowledge and experiences.",
-        topics: ["Python", "Programming", "Software Development"],
-        attending: [
-          { userName: "alice" },
-          { userName: "bob" },
-          { userName: "charlie" },
-        ],
-        size_limit: 6,
-      },
-      {
-        event_id: 3,
-        title: "JavaScript Workshop",
-        username: "js_guru",
-        event_img_url:
-          "https://miro.medium.com/v2/resize:fit:1200/1*BPSx-c--z6r7tY29L19ukQ.png",
-        location: "New York City",
-        date: "05/11/2023",
-        description:
-          "Enhance your JavaScript skills at our interactive JavaScript Workshop.",
-        topics: ["JavaScript", "Frontend", "Web Development"],
-        attending: [
-          { userName: "david" },
-          { userName: "emma" },
-          { userName: "frank" },
-          { userName: "grace" },
-        ],
-
-        size_limit: 6,
-      },
-      {
-        event_id: 4,
-        title: "Data Science Symposium",
-        username: "data_ninja",
-        event_img_url:
-          "https://uswfoxtail.blob.core.windows.net/foxtail-prod-uploads/images/Thinks.8dd35e85.fill-1366x700.format-jpeg.jpegquality-80.jpg",
-        location: "Chicago",
-        date: "20/11/2023",
-        description:
-          "Explore the latest trends and breakthroughs in Data Science at our Symposium.",
-        topics: ["Data Science", "Machine Learning", "Artificial Intelligence"],
-        attending: [
-          { userName: "hannah" },
-          { userName: "ian" },
-          { userName: "jack" },
-          { userName: "kate" },
-        ],
-        size_limit: 6,
-      },
-      {
-        event_id: 5,
-        title: "AI and Machine Learning Expo",
-        username: "ml_wizard",
-        event_img_url:
-          "https://assetsio.reedpopcdn.com/the-making-of-system-shock-2s-best-level-1504277832620.jpg?width=1200&height=1200&fit=bounds&quality=70&format=jpg&auto=webp",
-        location: "Seattle",
-        date: "08/12/2023",
-        description:
-          "Experience the cutting-edge advancements in AI and Machine Learning at our Expo.",
-        topics: ["Artificial Intelligence", "Machine Learning", "Technology"],
-        attending: [
-          { userName: "lily" },
-          { userName: "mike" },
-          { userName: "natalie" },
-          { userName: "oliver" },
-          { userName: "penny" },
-        ],
-        size_limit: 6,
-      },
-    ]);
+    getEventData().then((res) => {
+      setArticles(res);
+    });
   }, []);
 
   return (
@@ -137,7 +48,7 @@ export default function Home({ navigation }: any) {
             renderItem={({ item }) =>
               renderHorizontalItems({ item, navigation })
             }
-            keyExtractor={(item) => item.title}
+            keyExtractor={(item) => item._id}
             horizontal={true}
           ></FlatList>
         </View>
@@ -145,7 +56,7 @@ export default function Home({ navigation }: any) {
           <FlatList
             data={articles}
             renderItem={({ item }) => renderVerticalItems({ item, navigation })}
-            keyExtractor={(item) => item.title}
+            keyExtractor={(item) => item._id}
             horizontal={false}
           />
         </View>
