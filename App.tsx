1f8d25bf3757b129378da02ef6c99f9d38e3b204
Hassen-Ahmed <hassenbest23@gmail.com>
2023-08-07 14:11:57 +0100

    wrap navigation with context provider

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Navigation from "./src/navigation/Navigation";
+import { MyContextProvider } from "./src/contexts/Context";
 
 export default function App() {
-  return <Navigation />;
+  return (
+    <MyContextProvider>
+      <Navigation />
+    </MyContextProvider>
+  );
 }
 
 const styles = StyleSheet.create({
