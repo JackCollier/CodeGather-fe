6ae696d73e8149fd1ed2c399962654299330c6a6
JacvaSipt <steamcow12@hotmail.com>
2023-08-07 09:59:36 +0100

    create post function to hanndle log in data

--- src/utils/CodeGatherApi.tsx
+++ src/utils/CodeGatherApi.tsx
@@ -11,3 +11,19 @@ export const getEventDataById = async (Id: string) => {
   const data = await response.json();
   return data;
 };
+
+export const postLogin = async (email: string, password: string) => {
+  const response = await fetch(
+    `https://codegather.onrender.com/api/users/login`,
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    }
+  );
+  const data = await response.json();
+  return data;
+};
