159ed49638d89ea886ee0f6a4c39b7ea6b0d1a40
Hassen-Ahmed <hassenbest23@gmail.com>
2023-08-08 10:36:02 +0100

    reduce the limit to 300

--- src/utils/CityApi.tsx
+++ src/utils/CityApi.tsx
@@ -2,7 +2,7 @@ import { REST_API_KEY, APPLICATION_ID } from "@env";
 
 export const getCityData = async () => {
   const response = await fetch(
-    "https://parseapi.back4app.com/classes/Ukcities_City?count=1&limit=350&order=-population&keys=name,location",
+    "https://parseapi.back4app.com/classes/Ukcities_City?count=1&limit=300&order=-population&keys=name,location",
     {
       headers: {
         "X-Parse-Application-Id": `${APPLICATION_ID}`,
