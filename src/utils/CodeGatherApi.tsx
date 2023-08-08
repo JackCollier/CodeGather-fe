17442c26b25293eb08a3fb3facd7992490c9a134
Hassen-Ahmed <hassenbest23@gmail.com>
2023-08-08 11:41:35 +0100

    create body for header and type Event

--- src/utils/CodeGatherApi.tsx
+++ src/utils/CodeGatherApi.tsx
@@ -86,23 +86,45 @@ export const getProfileById = async (id: string) => {
   return data;
 };
 
+interface Event {
+  event_title: string;
+  location: string;
+  image: string;
+  date_time: string;
+  topics: string;
+  attending: string[];
+  description: string;
+  size_limit: string;
+}
+
 export const postEvent = async ({
   event_title,
+  location,
+  image,
   date_time,
   topics,
-  image,
-  location,
-  size_limit,
   attending,
   description,
-}) => {
+  size_limit,
+}: Event) => {
   const response = await fetch(`https://codegather.onrender.com/api/events`, {
     method: "POST",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({}),
+    body: JSON.stringify({
+      event: {
+        event_title,
+        location,
+        image,
+        date_time,
+        topics,
+        attending,
+        description,
+        size_limit,
+      },
+    }),
   });
   const data = await response.json();
   return data;
