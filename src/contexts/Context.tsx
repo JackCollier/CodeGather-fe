e1560d02dc92e06032e067dc39cc02cd45f6cd94
JacvaSipt <steamcow12@hotmail.com>
2023-08-09 10:04:04 +0100

    remove console log and useffect param

--- src/contexts/Context.tsx
+++ src/contexts/Context.tsx
@@ -12,17 +12,15 @@ export const MyContextProvider = ({ children }) => {
   useEffect(() => {
     AsyncStorage.getItem("profileId")
       .then((id) => {
+        console.log(id);
         const { profile_id } = JSON.parse(id);
-        console.log(profile_id);
-
         return getProfileById(profile_id);
       })
       .then((profile) => {
-        console.log("profile", profile);
         setProfileData(profile);
       })
       .catch((err) => {});
-  }, [isEventPosted]);
+  }, []);
 
   return (
     <MyContext.Provider
